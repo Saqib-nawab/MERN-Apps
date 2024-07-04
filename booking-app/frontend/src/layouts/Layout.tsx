28b7b68cd87a8dacff5d72ad0ef7b1f0a3948199
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-04 15:14:12 +0500

    added layout and header component

---
+++ booking-app/frontend/src/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+// import Footer from "../components/Footer";
+import Header from "../components/Header";
+// import Hero from "../components/Hero";
+// import SearchBar from "../components/SearchBar";
+
+
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default Layout;
