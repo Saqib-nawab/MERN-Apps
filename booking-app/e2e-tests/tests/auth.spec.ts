6dc9259320dc3ceaf0cf1d332f405f589b7ccc9b
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-10 12:49:41 +0500

    user auth tests

--- booking-app/e2e-tests/tests/auth.spec.ts
+++ booking-app/e2e-tests/tests/auth.spec.ts
@@ -1,18 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const UI_URL = "http://localhost:5173/";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+//user auth tests
+test("should allow the user to sign in", async ({ page }) => {
+  //test name
+  await page.goto(UI_URL);
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  // get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click(); //if page contains anything as sign in Link
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+  await page.getByRole("link", { name: "Sign In" }).click(); //if page contains anything as sign in Button
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.locator("[name=email]").fill("1@1.com"); //filling the form using PlayWright if any element with name email exists then fill it
+  await page.locator("[name=password]").fill("password123");
+
+  await page.getByRole("button", { name: "Login" }).click(); // if login button is exists
+
+  //after login button  is clicked does the following appears
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
